05cfe1dda5f123ec272763628595c7f20fb8f77e
Karl Factora <40439992+kfactora@users.noreply.github.com>
2019-12-17 15:04:09 -0800

    Update hello_world.py

--- python_fundamentals/hello_world.py
+++ python_fundamentals/hello_world.py
@@ -3,16 +3,16 @@ print("Hello World")
 
 # 2. print "Hello Noelle!" with the name in a variable
 name = "Noelle"
-print( your code here )	# with a comma
-print( your code here )	# with a +
+print("Hello", name)	# with a comma
+print("Hello" + name)	# with a +
 
 # 3. print "Hello 42!" with the number in a variable
-name = 42
-print( your code here )	# with a comma
-print( your code here )	# with a +	-- this one should give us an error!
+name = '42'
+print("Hello", name)	# with a comma
+print("Hello" + name)	# with a +	-- this one should give us an error!
 
 # 4. print "I love to eat sushi and pizza." with the foods in variables
-fave_food1 = "sushi"
-fave_food2 = "pizza"
-print( your code here ) # with .format()
-print( your code here ) # with an f string
+food1 = "sushi"
+food2 = "pizza"
+print("I love to eat {} and {}".format(food1, food2)) # with .format()
+print(f"I love to eat {food1} and {food2}.") # with an f string
