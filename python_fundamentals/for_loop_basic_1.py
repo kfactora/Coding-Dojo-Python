12d90035b6a509bdb889105def4596978942be8c
Karl Factora <40439992+kfactora@users.noreply.github.com>
2019-12-23 10:31:47 -0800

    Update for_loop_basic_1.py

--- python_fundamentals/for_loop_basic_1.py
+++ python_fundamentals/for_loop_basic_1.py
@@ -1,3 +1,21 @@
-Basic - Print all integers from 0 to 150.
+# 1. Basic - Print all integers from 0 to 150.
 for x in range(0, 150, 1)
 for x in range(150)
+
+# 2. Multiples of Five - Print all the multiples of 5 from 5 to 1,000
+for x in range(0, 1000, 5)
+
+# 3. Counting, the Dojo Way - Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
+for x in range(1, 100, 1)
+  if val 'divided by' 5 
+  print('Coding')
+  else: val 'divided by' 10
+  print('Coding Dojo')
+
+# 4. Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
+
+
+# 5. Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
+
+
+# 6. Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines
