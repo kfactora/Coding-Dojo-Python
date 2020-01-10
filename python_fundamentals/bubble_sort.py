25de94022146d79835f9ceca9488b9915e68eb7c
Karl Factora <40439992+kfactora@users.noreply.github.com>
2020-01-10 13:34:00 -0800

    Update bubble_sort.py

--- python_fundamentals/bubble_sort.py
+++ python_fundamentals/bubble_sort.py
@@ -3,3 +3,11 @@
 # Execute the famous Bubble Sort
 
 arr = [6,5,3,1,8,7,2,4]
+
+def bubleSort(arr):
+  for j in range(len(arr)-1):
+    for i in range(len(arr)-1-j):
+       if arr[i] > arr[i+1]:
+          arr[i], arr[i+1] = arr[i+1], arr[i]
+  return arr
+print(bubleSort(arr))
