73c9784d6ee34a6e9876111d9994c2906e2f4275
Karl Factora <40439992+kfactora@users.noreply.github.com>
2020-01-13 11:02:41 -0800

    Create bankAccount.py

---
+++ python_oop/bankAccount.py
@@ -0,0 +1,10 @@
+Assignment: BankAccount
+Objectives
+Practice writing classes
+
+The class should also have the following methods:
+
+deposit(self, amount) - increases the account balance by the given amount
+withdraw(self, amount) - decreases the account balance by the given amount if there are sufficient funds; if there is not enough money, print a message "Insufficient funds: Charging a $5 fee" and deduct $5
+display_account_info(self) - print to the console: eg. "Balance: $100"
+yield_interest(self) - increases the account balance by the current balance * the interest rate (as long as the balance is positive)
