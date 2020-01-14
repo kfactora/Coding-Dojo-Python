f20c440efe77718f2b61314d03423bfa3dc1b871
Karl Factora <40439992+kfactora@users.noreply.github.com>
2020-01-14 11:15:06 -0800

    Update math_dojo.py

--- python_oop/math_dojo.py
+++ python_oop/math_dojo.py
@@ -1,5 +1,33 @@
-Assignment: MathDojo
-Objectives:
-Practice creating a class and creating new instances
-Practice chaining methods
-Practice writing flexible functions that can take a varying number of arguments
+# Assignment: MathDojo
+# Objectives:
+# Practice creating a class and creating new instances
+# Practice chaining methods
+# Practice writing flexible functions that can take a varying number of arguments
+#Create a Python class called MathDojo that has one attribute, result, and 2 methods: add and subtract. The 2 methods each must take at least 1 parameter, but could take many more.
+
+class MathDojo(object):
+	def __init__(self): 
+		self.total = 0
+
+	def add(self, *num): 
+		for i in range(0, len(num)):
+			if type(num[i]) is list or type(num[i]) is tuple:
+				for j in num[i]: 
+					self.total += j
+			else:
+				self.total += num[i]
+		return self 
+
+	def subtract(self, *num): 
+		for i in range(0, len(num)):
+			if type(num[i]) is list or type(num[i]) is tuple:
+				for j in num[i]: 
+					self.total -= j
+			else:
+				self.total -= num[i]
+		return self
+
+	def result(self): 
+		print(self.total)
+
+md = MathDojo().add([1],2,3).add([4, 5, 6, 7], [8, 9.10, 11.12]).result()
