caf0f791e75e5f2902daecbfec8c622f68702691
Karl Factora <40439992+kfactora@users.noreply.github.com>
2020-01-14 10:29:59 -0800

    Create math_dojo.py

---
+++ python_oop/math_dojo.py
@@ -0,0 +1,5 @@
+Assignment: MathDojo
+Objectives:
+Practice creating a class and creating new instances
+Practice chaining methods
+Practice writing flexible functions that can take a varying number of arguments
