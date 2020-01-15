b040b31146dd11854781a57b515e6b55e1fbaca4
Karl Factora <40439992+kfactora@users.noreply.github.com>
2020-01-14 17:36:00 -0800

    Update singly_linked_lists.py

--- data_structures/singly_linked_lists.py
+++ data_structures/singly_linked_lists.py
@@ -1,18 +1,37 @@
 class SLNode:
-  def __init__(self, val):
-    self.value = val
-    self.next = None
+    def __init__(self, val):
+        self.value = val
+        self.next = None
   
 class SLList:
-  def __init__(self):
-    self.head = None
-  def addToFront(self, val):
-    new_node = SLNode(val)
-    current_head = self.head
-    new_node.next = current_head
-    self.head = new_node	
-    return self	      
+    def __init__(self):
+        self.head = None
+     
+    def add_to_front(self, val):
+        new_node = SLNode(val)
+        current_head = self.head
+        new_node.next = current_head
+        self.head = new_node
+        return self	                    	 
+              
+    def add_to_back(self, val):
+        if self.head == None:	
+            self.add_to_front(val)	
+        return self	 
 
-my_list = SLList()
-my_list.addToFront("Jim").addToFront("Dwight").addToFront("Andy")
-print(my_list)
+        new_node = SLNode(val)
+        runner = self.head
+        while (runner.next != None):
+            runner = runner.next
+        runner.next = new_node
+        return self  
+
+    def print_values(self):
+        runner = self.head
+        while (runner != None):
+            print(runner.value)
+        runner = runner.next 
+        return self            
+
+my_list = SList()	
+my_list.add_to_front("are").add_to_front("Linked lists").add_to_back("fun!").print_values()
