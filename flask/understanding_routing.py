59346c3f0880f0bf0823a515946253f468a6b535
Karl Factora <40439992+kfactora@users.noreply.github.com>
2020-01-28 15:32:50 -0800

    Update understanding_routing.py

--- flask/understanding_routing.py
+++ flask/understanding_routing.py
@@ -1,1 +1,25 @@
+from flask import Flask
+app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    print("initialized the WORLD!")
+    return 'Hello World!'
+
+@app.route('/dojo')
+def dojo():
+    print("initialized the DOJO!")
+    return 'Dojo!'
+
+@app.route('/say/<name>')
+def say(name):
+    print("initialized FLASK!")
+    return 'Hi '+name
+
+@app.route('/repeat/<number>/<name>')
+def repeat(name, number):
+    print("initialized the REPEAT!")
+    return name*int(number)
+
+if __name__=="__main__":
+    app.run(debug=True)
